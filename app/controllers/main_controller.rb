a9614a7fd0e40376e9d259365af392cc7a759cc5
Reynaldi Ananda Pane <reynaldipane@Reynaldis-MacBook-Pro.local>
2018-08-06 13:58:25 +0700

    [alam/rey] refactor main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,14 @@
+require 'httparty'
+
 class MainController < ApplicationController
   def index
+    response = HTTParty.get(
+      "http://172.28.128.5:9300/api/v1/container",
+      :headers => {
+        "Content-Type" => "application/json"
+      }
+    )
+
+    @lxclist = JSON.parse(response.body)
   end
 end
